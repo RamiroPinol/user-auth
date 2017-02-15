d24da1d549f0888c1a80d84e70d89de5383dbed9
RamiroPinol <ramirop@protonmail.com>
2017-02-15 20:15:59 -0300

    added views and partials and updated routes.js

--- app/routes.js
+++ app/routes.js
@@ -5,14 +5,24 @@ module.exports = function(app, passport) {
   });
 
   app.get('/login', function(req, res) {
-    res.render('login.ejs', { message: req.flash('loginMessage') });
+    res.render('login.ejs', {
+      message: req.flash('loginMessage'),
+      action: "Login",
+      alternative: "Signup",
+      text: "Need an account?"
+    });
   });
 
   // process the login form
   // app.post('/login', do all our passport stuff here);
 
   app.get('/signup', function(req, res) {
-    res.render('signup.ejs', { message: req.flash('signupMessage') });
+    res.render('signup.ejs', {
+      message: req.flash('signupMessage'),
+      action: "Signup",
+      alternative: "Login",
+      text: "Already have an account?"
+    });
   });
 
   // process the signup form
