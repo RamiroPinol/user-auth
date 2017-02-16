412c0348b554ec55cf9994465d825d7ddec86086
RamiroPinol <ramirop@protonmail.com>
2017-02-16 01:24:38 -0300

    Facebook authentication added

---
+++ app/lib/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  'facebookAuth' : {
+    'clientID'      : '391008777930174',
+    'clientSecret'  : '3fc71e7ab6e0956d32979b9c75349aa2', // your App Secret
+    'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+  },
+
+  'twitterAuth' : {
+    'consumerKey'       : 'your-consumer-key-here',
+    'consumerSecret'    : 'your-client-secret-here',
+    'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+  },
+
+  'googleAuth' : {
+    'clientID'      : 'your-secret-clientID-here',
+    'clientSecret'  : 'your-client-secret-here',
+    'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+  }
+
+};
