94d854280a4c573319bf7578d7fd8d11ab6c0f1c
RamiroPinol <ramirop@protonmail.com>
2017-02-16 13:27:45 -0300

    added authentification for Twitter and Google

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -3,19 +3,19 @@ module.exports = {
   'facebookAuth' : {
     'clientID'      : '391008777930174',
     'clientSecret'  : '3fc71e7ab6e0956d32979b9c75349aa2', // your App Secret
-    'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+    'callbackURL'   : 'http://localhost:4000/auth/facebook/callback'
   },
 
   'twitterAuth' : {
-    'consumerKey'       : 'your-consumer-key-here',
-    'consumerSecret'    : 'your-client-secret-here',
-    'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+    'consumerKey'       : 'GM61lAfyvMxpItno1adE8UtQ7',
+    'consumerSecret'    : '3UrjdhNEAD1kctXbBsjjMMtoKQSyPxS8nisaaFyx7jhDSzkUQd',
+    'callbackURL'       : 'http://localhost:4000/auth/twitter/callback'
   },
 
   'googleAuth' : {
-    'clientID'      : 'your-secret-clientID-here',
-    'clientSecret'  : 'your-client-secret-here',
-    'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    'clientID'      : '391993236540-dl6u7n7btlbfr2fsccs0jtrumm0vefhe.apps.googleusercontent.com',
+    'clientSecret'  : 'AGFHWUEeAcFQSXgCMAj0meTu',
+    'callbackURL'   : 'http://localhost:4000/auth/google/callback'
   }
 
 };
